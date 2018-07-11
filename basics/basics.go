c970b74a576b7be1ea3f15dc72188e8bb8a9232a
Frank Larios <frankandrewlarios@gmail.com>
2018-07-11 12:46:12 -0700

    Adds commented out file of golang basics

---
+++ basics/basics.go
@@ -0,0 +1,58 @@
+package main
+
+func main() {
+
+	// Can't do real math with builtin primitive types!!!
+	/*
+		a := 2.0
+		b := 3.0
+		r := a / b
+
+		fmt.Printf("%.20f\n", r)
+	*/
+
+	/* booleans
+	var b bool = true
+	fmt.Println(b)
+	*/
+
+	/*
+		helloWorld := "Hello World"
+		fmt.Println(helloWorld)
+	*/
+
+	// Infinite loop
+	/*
+		for {
+			fmt.Println("Hello World")
+		}
+	*/
+
+	// Golang's equivalent of "while" loop
+	/*
+		i := 0
+		for i < 10 {
+			fmt.Println("Hello World:", i)
+			i = i + 1
+		}
+	*/
+
+	// for
+	/*
+		for i := 0; i < 10; i++ {
+			fmt.Println("Hello World:", i)
+		}
+	*/
+
+	// Conditionals
+	/*
+		x := 5
+		if x > 5 {
+			fmt.Println("Hello world")
+		} else if x < 5 {
+			fmt.Println("Goodbye")
+		} else {
+			fmt.Println("IT IS FIVE")
+		}
+	*/
+}
