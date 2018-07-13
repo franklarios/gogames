c7d6cdbc55a03b9d4da8b8f2c2ffbb883564e2a8
Frank Larios <frankandrewlarios@gmail.com>
2018-07-12 22:34:07 -0700

    Adds basic text adventure which implements a Linked List data structure

---
+++ linearStory/linearStory.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+// add a function that will insert a new page, after a given page
+// add a function that will delete a page
+
+type storyPage struct {
+	text     string
+	nextPage *storyPage
+}
+
+func playStory(page *storyPage) {
+	if page == nil {
+		return
+	}
+	fmt.Println(page.text)
+	playStory(page.nextPage)
+}
+
+func main() {
+
+	page1 := storyPage{"It was a dark and stormy night.", nil}
+	page2 := storyPage{"You are alone, and you need to find the sacred helmet before the bad guys do", nil}
+	page3 := storyPage{"You see a troll ahead", nil}
+	page1.nextPage = &page2
+	page2.nextPage = &page3
+
+	playStory(&page1)
+}
